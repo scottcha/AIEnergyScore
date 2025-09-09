81d6ddfcb21c107c98cb4794110e1403c1bc497e
Sasha Luccioni <sasha.luccioni@huggingface.co>
2025-09-09 11:37:30 -0400

    Update entrypoint.sh
    updating dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python /check_h100.py
 if [[ $? = 0 ]]; then
     mkdir -p "${RESULTS_DIR}"
     # Force Hydra (used by optimum-benchmark) to write outputs into /results
-    optimum-benchmark --config-dir /optimum-benchmark/examples/energy_star/ "$@" hydra.run.dir="${RESULTS_DIR}"
+    optimum-benchmark --config-dir /optimum-benchmark/energy_star/ "$@" hydra.run.dir="${RESULTS_DIR}"
     # Post-run summarizer: reads benchmark_report.json and prints/writes GPU_ENERGY_WH
     python /summarize_gpu_wh.py "${RESULTS_DIR}"
 fi
